7e20d5d02f9dd87f130d0ad16f658d59a36b1436
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-12-01 18:19:17 +0800

    Update DifficultQuestionsOnly.sql

--- DifficultQuestionsOnly.sql
+++ DifficultQuestionsOnly.sql
@@ -1,7 +1,15 @@
 
 Q28
-
 select ShipCountry, avg(Freight) from Orders 
 where Date(OrderDate) between (select date_sub(max(OrderDate), Interval 1 year) from Orders) and (select max(OrderDate) from Orders) 
 group by ShipCountry order by avg(Freight) desc limit 3
 
+Q32 (not difficult, just annoying)
+select c.CustomerID, c.CompanyName from Customers c
+join Orders o on c.CustomerID = o.CustomerID
+ where o.OrderID in 
+  (select o.OrderID from OrderDetails d
+  join Orders o on o.OrderID = d.OrderID
+  where o.OrderDate like "2016%"
+  group by o.OrderId having sum(UnitPrice*Quantity)>10000)
+order by c.CustomerID
